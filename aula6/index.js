c41e8a1275dbf07e3c2df7a0c1147b58563a01af
Marcelle Lima <marcellesilva0101@outlook.com>
2022-07-20 10:23:53 -0300

    aula 6

---
+++ aula6/index.js
@@ -0,0 +1,10 @@
+//NÃO USAR VAR, USAR LET! se for variar.
+
+let nome = "Marcelle";
+console.log(nome, 'é legal');
+
+let x; //Declarar a varivael
+x = 'qualquer valor'; //INicializar a variavel
+console.log(x);
+
+let nomeCompleto = 'Marcelle Lima' //camelCase, case-sensitive (maiusculo e minisculo faz a diferença)
