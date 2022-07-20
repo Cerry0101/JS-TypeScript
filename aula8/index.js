f00c1095bbd78b8a0dc7842e3fb55879913d4887
Marcelle Lima <marcellesilva0101@outlook.com>
2022-07-20 12:49:11 -0300

    aula 8

---
+++ aula8/index.js
@@ -0,0 +1,11 @@
+const nome = "Marcelle";
+const sobrenome = "Lima";
+const idade = "22";
+const peso = 80;
+const altura = 1.64;
+let imc; //peso / (altura * altura)
+let anoNacimento;
+
+
+let resultado = peso / (altura * altura);
+console.log(nome + sobrenome);
